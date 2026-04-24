a5e31e8020f2d55cebefcbe8596cb0bb7b5b1346
Warren Watson <wwatson4506@gmail.com>
2026-04-24 11:27:20 -0700

    Update secrets.h

--- src/qnethernet/drivers/cyw4343w/src/secrets.h
+++ src/qnethernet/drivers/cyw4343w/src/secrets.h
@@ -1,10 +1,10 @@
 // The secrets file
 
 // SSID
-#define MY_SSID            "wswn"
+#define MY_SSID            "testSSID"
 
 //PASSPHRASE
-#define MY_PASSPHRASE      "E591E7199F5C3B834FC6C2D810A6E724D4D60FF48696768D0AA41353905A320"
+#define MY_PASSPHRASE      "testPHRASE"
 
 // Security settings: 0 for none, 1 for WPA_TKIP, 2 for WPA2
 // The hard-coded password is for test purposes only!!!
